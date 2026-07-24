842b0599bde1c35e751f84ed8dc715ae5c5af369
Simplicwhats <levisd2104@gmail.com>
2026-07-24 14:17:06 -0300

    Delete client directory

--- client/src/assets/utils.ts
+++
@@ -1,8 +0,0 @@
-import type { GameMod } from './types'
-
-const DEFAULT_IMAGE = 'data/logo_1024.png'
-export function getModImage(mod: GameMod): string {
-  const { id, image } = mod
-  if (image == null) return DEFAULT_IMAGE
-  return `packages/textures/images/${image}`
-}
